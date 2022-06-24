8cd0bc763748f8b4dc81854598bfc10bffe5e485
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-06-24 10:40:03 +0200

    fixing unused content error when loading nimbus page

--- content.js
+++ content.js
@@ -7,26 +7,21 @@ const hideElement = (element) => {
 try {
   const title = document.querySelector('body > div > table > tbody > tr > td:nth-child(2) > div:nth-child(1) > table > tbody > tr:nth-child(3) > td > div > table > tbody > tr:nth-child(1) > td > p > font:nth-child(1)')
   title.classList.add('title')
-  
-  const subtitle = document.querySelector('body > div > table > tbody > tr > td:nth-child(2) > div:nth-child(1) > table > tbody > tr:nth-child(3) > td > div > table > tbody > tr:nth-child(1) > td > p > font:nth-child(2) > font > font:nth-child(3)')
-  subtitle.classList.add('subtitle')
-  const dateEmitted = document.querySelector("body > div > table > tbody > tr > td:nth-child(2) > div:nth-child(1) > table > tbody > tr:nth-child(3) > td > div > table > tbody > tr:nth-child(1) > td > p > font:nth-child(2) > font > font:nth-child(5)")
-  dateEmitted.classList.add('date-emitted')
-  
+
   const tableElements = document.body.getElementsByTagName('table')
   const mainTable = tableElements[0]
   const leftColumn = mainTable.getElementsByTagName('td')[0]
-  
+
   hideElement(leftColumn)
-  
+
   const bodyTable = tableElements[3]
   const bodyRows = bodyTable.children[0].children
   const bodyNavbar = bodyRows[0]
   const bodyPromotion = bodyRows[1]
-  
+
   hideElement(bodyNavbar)
   hideElement(bodyPromotion)
-  
+
   const satImgRow = document.querySelector("body > div > table > tbody > tr > td:nth-child(2) > div:nth-child(1) > table > tbody > tr:nth-child(3) > td > div > table > tbody > tr:nth-child(4)")
   hideElement(satImgRow)
 
